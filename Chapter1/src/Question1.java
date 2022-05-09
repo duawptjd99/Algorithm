6533013e41d9470f04e887c7891ad25f2cb7e351
duawptjd99 <48855410+duawptjd99@users.noreply.github.com>
2022-05-09 17:13:50 +0900

    백준 1단계 문제
    커밋 테스트

---
+++ Chapter1/src/Question1.java
@@ -0,0 +1,6 @@
+public class Question1 {
+    //Hello World!를 출력하시오.
+    public static void main(String args[]) {
+        System.out.print("Hello World!");
+    }
+}
---
+++ Chapter1/src/Question8.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Question8 {
+    public static void main(String args[]) {
+        Scanner scan = new Scanner(System.in);
+        float a = scan.nextInt();
+        float b = scan.nextInt();
+        scan.close();
+        System.out.println(a/b);
+    }
+}
